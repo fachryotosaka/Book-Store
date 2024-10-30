6b5d60df571700519985a449bcaf20ddaacc17b1
TestSubject71 <ethosislami07@gmail.com>
2024-10-30 14:04:46 +0700

    getNewId func, not tested yet

---
+++ src/book.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAX_LENGTH 100
+
+int getNewId(FILE* file){
+    int maxId = 0;
+    char line [MAX_LENGTH];
+
+    // Loop setiap line di file
+    while(fgets(line, sizeof(line), file) !=NULL){
+        
+        // Remove spaces/newline
+        line[strcspn(line, "\n")] = '\0';
+
+        // Seperate string, ambil id
+        char *idStr = strtok(line, "#");
+
+        if(*idStr != NULL){
+            // Parse id ke int
+            int id = atoi(idStr);
+
+            //Check dan replace max id dengan id terbesar
+            if(maxId < id){
+                maxId = id;
+            } 
+        }
+
+    }
+
+    //Return id terbesar + 1
+    return ++maxId;
+
+}
